b837cdcbaef692c87333f94cef52627412176467
natwocht <natwocht@gmail.com>
2018-03-25 14:29:37 +0200

    Initial commit

---
+++ src/main/java/pik/pikserver/PikServerApplication.java
@@ -0,0 +1,12 @@
+package pik.pikserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikServerApplication.class, args);
+	}
+}
